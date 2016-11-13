e89a947d0be92bd2ed40318fb43a534a0e9e193c
Martin Kierkegaard <mark@easj.dk>
2016-11-13 20:44:10 +0100

    Add project files.

---
+++ Sandbox/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    abstract class Employee
+    {
+        private String name;
+
+        public Employee(String name)
+        {
+            this.name = name;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+
+        public abstract int GetSalaryPerMonth();
+
+        public abstract int GetBonusPerMonth();
+
+        public abstract bool IsBonusPaidOut();
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            // Insert your tests here...
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
